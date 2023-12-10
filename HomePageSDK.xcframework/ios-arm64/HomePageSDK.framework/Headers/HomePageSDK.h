228d3ac0ac28423ba373381ca619f0fd16b7ec56
Abdul Mottaleb <mottalebgakk@gmail.com>
2023-12-10 11:57:13 +0600

    Initial Commit

---
+++ HomePageSDK.xcframework/ios-arm64/HomePageSDK.framework/Headers/HomePageSDK.h
@@ -0,0 +1,18 @@
+//
+//  HomePageSDK.h
+//  HomePageSDK
+//
+//  Created by Gakk on 7/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HomePageSDK.
+FOUNDATION_EXPORT double HomePageSDKVersionNumber;
+
+//! Project version string for HomePageSDK.
+FOUNDATION_EXPORT const unsigned char HomePageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomePageSDK/PublicHeader.h>
+
+
---
+++ SdkExperimentHP/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SdkExperimentHP
+//
+//  Created by Gakk on 10/12/23.
+//
+
+import UIKit
+import HomePageSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func ClickMe(_ sender: Any) {
+        
+        debugPrint("It Clicked me..!")
+        CoreClass.shared.gotoHomeVC(nav: self.navigationController)
+        
+    }
+    
+    
+}
+
